f387c8deb3d6cd65c32d652ee9e902b23f5d2562
Tsubasa <tsubasanikander@gmail.com>
2022-08-26 17:26:27 +0200

    Add basic themes

--- src/components/styles/Theme.styled.jsx
+++ src/components/styles/Theme.styled.jsx
@@ -1,15 +1,71 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 
-const theme = {
-    colors: {
-    },
-    fonts: {
-    },
-    fontSizes: {
-    }
+const fonts = {
 };
 
-export default function Theme({ children }) {
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+const spacing = {
+    xs: '0.5rem',
+    sm: '1rem',
+    md: '2rem',
+};
+
+const common = {
+    fonts,
+    spacing,
+};
+
+////
+//  Light
+///
+
+const lightThemeColors = {
+    white: '#FAF7F5',
+    grey: '#EBE6E4',
+    black: '#1A0D06',
+    black70: 'rgba(26, 13, 6, 70%)',
+    black40: 'rgba(26, 13, 6, 40%)',
+    black20: 'rgba(26, 13, 6, 20%)',
+    primary: '#F16E28',
+    primary70: '#F18348',
+    primary50: '#F19B6C',
+    primary20: '#F1D2C1',
+};
+
+const light = {
+    ...common,
+    colors: lightThemeColors,
+    fontColor: lightThemeColors.black,
+};
+
+////
+//  Dark
+///
+
+const darkThemeColors = {
+    white: '#FAF8F5',
+    grey: '#EBE8E4',
+    black: '#1A1406',
+    black70: 'rgba(26, 20, 6, 70%)',
+    black40: 'rgba(26, 20, 6, 40%)',
+    black20: 'rgba(26, 20, 6, 20%)',
+    primary: '#F1B228',
+    primary70: '#F1BC48',
+    primary50: '#F1CB79',
+    primary20: '#F1E2C1',
+};
+
+const dark = {
+    ...common,
+    colors: darkThemeColors,
+    fontColor: darkThemeColors.white,
+};
+
+export default function Theme({ theme, children }) {
+
+    return (
+        <ThemeProvider theme={theme === 'dark' ? dark : light}>
+            {children}
+        </ThemeProvider>
+    );
 }
