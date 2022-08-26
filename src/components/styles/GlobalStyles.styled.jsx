64a69dc1c68414c2a0fdaebd2046f865b267fdec
Tsubasa <tsubasanikander@gmail.com>
2022-08-26 17:26:07 +0200

    Add typography

--- src/components/styles/GlobalStyles.styled.jsx
+++ src/components/styles/GlobalStyles.styled.jsx
@@ -1,22 +1,75 @@
 import { createGlobalStyle } from 'styled-components';
 
-export default function GlobalStyle() {
-    createGlobalStyle`
-        * {
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-        }
-        body {
-        }
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *:after,
+    *::before {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-        section {
-        }
+    body {
+        overflow-x: hidden;
+        background-color: ${({ theme, mode }) =>
+        mode === 'dark' ?
+            theme.colors.black :
+            theme.colors.white
+    };
+    }
 
-        nav {
-        }
+    section {
+    }
 
-        footer {
-        }
+    header {
+
+    }
+
+    footer {
+    }
+
+    ///
+    // Typography
+    ///
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    button {
+        font-family: 'Josefin Sans', sans-serif;
+        font-weight: 700;
+
+        text-transform: capitalize;
+        font-family: 'Josefin Sans', sans-serif;
+    }
+
+    h1 {
+        font-size: 3rem;
+    }
+
+    h2 {
+        font-size: 2.4rem;
+    }
+
+    h3 {
+        font-size: 2rem;
+    }
+
+    h4 {
+        font-weight: 400;
+        font-size: 1.6rem;
+    }
+
+    h5,
+    button {
+        font-weight: 600;
+        font-size: 1.3rem;
+    }
+
+    p {
+        font-family: 'Lato', sans-serif;
+        font-weight: 400;
+    }
+
     `;
-}
