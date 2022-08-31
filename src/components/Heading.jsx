e95d9008d56eefc5928d0eaa90b314ccb39dce7e
Tsubasa <tsubasanikander@gmail.com>
2022-08-31 10:32:11 +0200

    Add H2 comp

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,9 @@
 import { motion } from 'framer-motion';
 import React from 'react';
+import { useContext } from 'react';
 import { forwardRef } from 'react';
 import styled from 'styled-components';
+import { ThemeContext } from './styles/ContextProviders';
 
 const Wrapper = styled.h1.attrs(({ size }) => (
     size ?
@@ -10,6 +12,21 @@ const Wrapper = styled.h1.attrs(({ size }) => (
 ))`
     color: ${({ theme }) => theme.fontColor};
     text-transform: capitalize;
+`;
+
+const headingMotion = {
+    visible: { left: '-1.5rem', opacity: 1 },
+    hidden: { left: 0, opacity: 0 }
+}
+
+const H2Wrapper = styled(motion.h2)`
+    color: ${({ theme }) => theme.fontColor};
+    text-transform: capitalize;
+    position: absolute;
+    top: 0;
+    text-transform: uppercase;
+    color: ${({ theme, $currentTheme }) =>
+        $currentTheme === 'dark' ? theme.colors.white : theme.colors.black};
 `;
 
 export function Heading(props) {
@@ -26,16 +43,21 @@ export function Heading(props) {
     )
 }
 
+export function H2(props) {
+    const { isVisible, children } = props;
+    const { currentTheme } = useContext(ThemeContext);
 
-export function MotionHeading(props) {
-    const { size, children, transform, className } = props;
-    const MotionWrapper = motion(Wrapper);
     return (
-        <MotionWrapper
-            size={size}
-            className={className}
-            $transform={transform}>
+        <H2Wrapper
+            $currentTheme={currentTheme}
+            animate={isVisible ? "visible" : "hidden"}
+            variants={headingMotion}
+            transition={{
+                ease: 'linear',
+                duration: 0.2
+            }}
+        >
             {children}
-        </MotionWrapper>
+        </H2Wrapper>
     )
-}
+};
