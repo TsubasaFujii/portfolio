4eb572c61f12c0dc6f4cdd2a0274a7be76e5eb9c
Tsubasa <tsubasanikander@gmail.com>
2022-09-08 18:05:27 +0200

    Change structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { GlobalStyles } from './components/styles/GlobalStyles.styled';
 import Theme from './components/styles/Theme.styled';
-import { ScreenSizeContext, ThemeContext } from './components/styles/ContextProviders';
+import { ThemeContext } from './components/styles/ContextProviders';
 import { useSwitchTheme } from './hooks/theme';
-import { useScreenSize } from './hooks/viewport';
+import { getPointingMethod } from './js/device';
 
 import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import { useTrackViewport } from './hooks/viewport';
 
+const links = [
+    {
+        name: 'Home',
+        route: '/',
+    }, {
+        name: 'Projects',
+        route: '/',
+    }, {
+        name: 'Contact',
+        route: '#contactForm',
+    }
+];
 
 function App() {
+    const [headerHeight, setHeaderHeight] = useState(null);
+    //const [currentSection, setCurrentSection] = useState(0);
+    const headerRef = useRef(null);
+
     const { currentTheme, toggleTheme } = useSwitchTheme();
-    const { screenSize } = useScreenSize();
+    const { pointingMethod } = getPointingMethod();
+
+    useEffect(() => {
+        if (!headerRef.current) return;
+        function handleResize() {
+            setHeaderHeight(headerRef.current.clientHeight);
+        }
+
+        setHeaderHeight(headerRef.current.clientHeight);
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [headerRef]);
+
+    const { currentSection, refs } = useTrackViewport(links);
+    const { contactRef } = refs;
 
     return (
         <Theme theme={currentTheme}>
-            <ThemeContext.Provider value={{ currentTheme, toggleTheme }}>
-                <ScreenSizeContext.Provider value={screenSize}>
-                    <GlobalStyles />
-                    <Home />
-                </ScreenSizeContext.Provider>
+            <ThemeContext.Provider value={{ currentTheme, toggleTheme, pointingMethod }}>
+                <GlobalStyles />
+                <Header ref={headerRef} current={currentSection} links={links} />
+                <Routes>
+                    <Route path="/" element={<Home headerHeight={headerHeight} />} />
+                </Routes>
+                <Footer contactRef={contactRef} />
             </ThemeContext.Provider>
         </Theme>
     );
