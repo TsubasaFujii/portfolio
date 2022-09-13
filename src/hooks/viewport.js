ea1281e2446f58c98d9d125c78d4a55e9797425c
Tsubasa <tsubasanikander@gmail.com>
2022-09-13 09:18:21 +0200

    Move data to content.js

--- src/hooks/viewport.js
+++ src/hooks/viewport.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useLocation } from 'react-router-dom';
 
@@ -15,43 +15,6 @@ export const devices = {
     desktop: `(min-width: ${breakpoints.lg}px)`,
     desktopL: `(min-width: ${breakpoints.xl}px)`,
 };
-
-
-function getScreenSize() {
-    const width = window.innerWidth;
-    const {sm, md, lg} = breakpoints;
-    if (width < sm) {
-        return 'sm';
-    } else if (width < md) {
-        return 'md';
-    } else if (width < lg) {
-        return 'lg';
-    } else {
-        return 'xl';
-    }
-}
-
-export function useScreenSize() {
-    const [screenSize, setScreenSize] = useState(getScreenSize);
-
-    const handleResize = useCallback(() => {
-        const currentScreenSize = getScreenSize();
-
-        if (screenSize === currentScreenSize) return;
-
-        setScreenSize(currentScreenSize);
-    }, [screenSize]);
-
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-
-        return () => window.addEventListener('resize', handleResize);
-    }, [handleResize]);
-
-    return {
-        screenSize,
-    }
-}
 
 export function useTrackViewport(links) {
     // links[0] is default
