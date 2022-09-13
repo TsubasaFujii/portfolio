3002928da98a57a0bbd47bf806cd8de8707dbc6a
Tsubasa <tsubasanikander@gmail.com>
2022-09-13 22:02:43 +0200

    Move Container comp outside the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import Hero from '../components/layouts/Hero';
-import AboutMe from '../components/layouts/AboutMe';
-import Projects from '../components/layouts/Projects';
-import { devices } from '../hooks/viewport';
-
-const Container = styled.main`
-    padding: ${({ theme }) => `0 ${theme.spacing.md}`};
-    z-index: 200;
-    
-    display: flex;
-    flex-direction: column;
-    gap: ${({ theme }) => theme.spacing.gap};
-
-    & section:last-child {
-        margin-bottom: ${({ theme }) => theme.spacing.gap};
-    }
-    
-    @media screen and (${devices.mobileL}) {
-        padding: ${({ theme }) => `0 ${theme.spacing.xl}`};
-    }
-`;
+import Hero from '../components/layouts/index/Hero';
+import AboutMe from '../components/layouts/index/AboutMe';
+import HighlightedProjects from '../components/layouts/index/HighlightedProjects';
 
 export default function Home(props) {
     const { headerHeight } = props;
 
     return (
-        <Container headerHeight={headerHeight}>
+        <>
             <Hero headerHeight={headerHeight} />
             <AboutMe />
-            <Projects />
-        </Container>
+            <HighlightedProjects />
+        </>
     )
 }
 
