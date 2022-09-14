e3277490793e847bfb02ec9f1ff572b6a3646d44
Tsubasa <tsubasanikander@gmail.com>
2022-09-14 09:29:00 +0200

    Add Main comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+import Main from '../components/layouts/Main';
 import Hero from '../components/layouts/index/Hero';
 import AboutMe from '../components/layouts/index/AboutMe';
 import HighlightedProjects from '../components/layouts/index/HighlightedProjects';
+
+const Container = styled(Main)`
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.spacing.gap};
+`;
 
 export default function Home(props) {
     const { headerHeight } = props;
 
     return (
-        <>
+        <Container headerHeight={headerHeight}>
             <Hero headerHeight={headerHeight} />
             <AboutMe />
             <HighlightedProjects />
-        </>
+        </Container>
     )
 }
 
