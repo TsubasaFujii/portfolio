9507f445e14a9aa75997e27bbfe2132431102c31
Tsubasa <tsubasanikander@gmail.com>
2022-09-14 09:28:25 +0200

    Add comp Main and Page, main comp is dynamic

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -1,23 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react'
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-import { links } from '../../data/content';
-import { devices, useTrackViewport } from '../../hooks/viewport';
-
-import { Header } from './Header';
-import Home from '../../pages/Home';
-import Projects from '../../pages/Projects';
-import Footer from './Footer';
+import { devices } from '../../hooks/viewport';
 
 const Container = styled.main`
     margin-top: ${({ $headerHeight }) => `${$headerHeight}px`};
     padding: ${({ theme }) => `0 ${theme.spacing.md}`};
+    position: relative;
     z-index: 200;
-
-    display: flex;
-    flex-direction: column;
-    gap: ${({ theme }) => theme.spacing.gap};
 
     & section:last-child {
         margin-bottom: ${({ theme }) => theme.spacing.gap};
@@ -28,35 +19,20 @@ const Container = styled.main`
     }
 `;
 
-export default function Main() {
-    const [headerHeight, setHeaderHeight] = useState(null);
-    const headerRef = useRef(null);
-
-    const { currentSection, refs } = useTrackViewport(links);
-    const { contactRef } = refs;
-
-    useEffect(() => {
-        if (!headerRef.current) return;
-        function handleResize() {
-            setHeaderHeight(headerRef.current.clientHeight);
-        }
-
-        setHeaderHeight(headerRef.current.clientHeight);
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, [headerRef]);
-
+export default function Main(props) {
+    const { children, headerHeight, className } = props;
     return (
-        <>
-            <Header ref={headerRef} current={currentSection} links={links} />
-            <Container $headerHeight={headerHeight}>
-                <Routes>
-                    <Route path="/" element={<Home headerHeight={headerHeight} />} />
-                    <Route path="/projects" element={<Projects headerHeight={headerHeight} />} />
-                </Routes>
-            </Container>
-            <Footer contactRef={contactRef} />
-        </>
+        <Container $headerHeight={headerHeight} className={className}>
+            {children}
+        </Container>
     )
 }
+
+Main.propTypes = {
+    headerHeight: PropTypes.number,
+    className: PropTypes.string,
+    children: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.string,
+    ]),
+};
