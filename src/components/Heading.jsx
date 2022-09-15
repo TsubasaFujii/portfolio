7e8784c458575f58c13ca156a5ba29a1f1a4872e
Tsubasa <tsubasanikander@gmail.com>
2022-09-15 12:38:35 +0200

    Add h3

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 import { ThemeContext } from './styles/ContextProviders';
 
@@ -20,6 +20,47 @@ const H2Wrapper = styled(motion.h2)`
     text-transform: capitalize;
     text-transform: uppercase;
     text-align: left;
+`;
+
+const H3Wrapper = styled(motion.h3)`
+    position: relative;
+    right: ${({ theme }) => `-${theme.spacing.sm}`};
+    padding-right: ${({ theme }) => `calc(${theme.spacing.sm} + 0.5em)`};
+    color: ${({ theme }) => theme.fontColor};
+    text-transform: capitalize;
+    text-align: left;
+`;
+
+const expand = keyframes`
+    0% {
+        transform: scaleX(0%);
+    }
+    100% {
+      transform: scaleX(100%) skew(-12deg);
+    }
+`;
+
+const AnimatedUnderline = styled(motion.span)`
+    position: relative;
+    overflow: hidden;
+
+    &:before {
+        content: '';
+        height: 0.5em;
+        width: 100%;
+        display: block;
+        
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        // HEX 45% = 73
+        background: ${({ theme }) => `${theme.colors.primary50}73`};
+        z-index: -1;
+    }
+
+    &.shown:before {
+        animation: ${expand} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards;
+    }
 `;
 
 export function Heading(props) {
@@ -71,6 +112,27 @@ export function H2(props) {
 }
 
 H2.propTypes = {
+    isVisible: PropTypes.bool,
+    children: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.string,
+    ]),
+};
+
+export function H3(props) {
+    const { children, isVisible } = props;
+    return (
+        <H3Wrapper
+            initial={{ left: 0, }}
+            animate={{
+                opacity: isVisible ? 1 : 0,
+            }}>
+            <AnimatedUnderline className={isVisible ? 'shown' : null}>{children}</AnimatedUnderline>
+        </H3Wrapper>
+    )
+}
+
+H3.propTypes = {
     isVisible: PropTypes.bool,
     children: PropTypes.oneOfType([
         PropTypes.array,
