5c2d7bfb5168e0ece9f0795795c21e9722df6ccc
Tsubasa <tsubasanikander@gmail.com>
2022-09-16 16:16:36 +0200

    Change the name

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,30 @@
+import React, { forwardRef } from 'react';
+import { useContext } from 'react';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
+import { ThemeContext } from './styles/ContextProviders';
+
+const OverlayWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: ${({ theme, $currentTheme }) =>
+        $currentTheme === 'dark' ? `${theme.colors.white}CC` : theme.colors.black40};
+
+    &:after {
+        content: '.';
+        visibility: hidden;
+    }
+`;
+
+export const Overlay = forwardRef((_, ref) => {
+    const { currentTheme } = useContext(ThemeContext);
+
+    return createPortal((
+        <OverlayWrapper id='modal' ref={ref} $currentTheme={currentTheme} />
+    ), document.querySelector('main'));
+});
+
+Overlay.displayName = 'Overlay';
