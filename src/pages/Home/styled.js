2d8084dff548e2147a0e1f811dc7497c53c6019b
Tsubasa <tsubasanikander@gmail.com>
2022-10-03 10:43:28 +0200

    Change container name

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { devices } from '../../hooks/viewport';
 
-export const Container = styled.main`
+export const Main = styled.main`
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.spacing.gap};
