e62b6a97a535062c2970feb483b1b7f3a2d6debe
Tsubasa <tsubasanikander@gmail.com>
2022-10-07 20:04:08 +0200

    Create useJumpTo

--- src/layouts/Footer/FooterLinks/FooterLinks.jsx
+++ src/layouts/Footer/FooterLinks/FooterLinks.jsx
@@ -1,19 +1,20 @@
 import { FooterLinksHeading, List, ListItem, Wrapper } from './styled';
-import { Link } from 'react-router-dom';
-
+import { useJumpTo } from '../../../hooks/route';
 import { links as pageList } from '../../../data/content';
 
 const pages = pageList.filter(({ route }) => route.charAt(0) === '/');
 
 export default function FooterLinks() {
+    const { jumpTo } = useJumpTo();
+
     return (
         <Wrapper>
             <FooterLinksHeading>Pages</FooterLinksHeading>
             <List>
                 {
                     pages.map(({ route, name }) => (
-                        <ListItem key={name}>
-                            <Link to={route}>{name}</Link>
+                        <ListItem key={name} onClick={() => jumpTo(route)}>
+                            {name}
                         </ListItem>
                     ))
                 }
