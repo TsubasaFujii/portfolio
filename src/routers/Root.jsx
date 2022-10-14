96730275e49383fbf263f055cbbdc3e810f26ee3
Tsubasa <tsubasanikander@gmail.com>
2022-10-14 14:01:12 +0200

    init

---
+++ src/routers/Root.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from 'react-router-dom';
+import { useRef } from 'react';
+
+import { useTrackViewport } from '../hooks/useTrackViewport';
+import { ThemeProvider } from '../context';
+import { links } from '../data/content';
+
+import { GlobalStyles } from '../components/styles/GlobalStyles';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
+
+export default function Root() {
+    const headerRef = useRef(null);
+    const { currentSection, refs } = useTrackViewport(links);
+    const { contactRef } = refs;
+    return (
+
+        <ThemeProvider>
+            <GlobalStyles />
+            <Header ref={headerRef} current={currentSection} links={links} />
+            <Outlet />
+            <Footer contactRef={contactRef} />
+        </ThemeProvider>
+    )
+}
