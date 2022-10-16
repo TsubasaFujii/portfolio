11fb56c99038326b6526ee18447f2b97ef2591bb
Tsubasa <tsubasanikander@gmail.com>
2022-10-16 13:05:13 +0200

    Update the logic to track viewport

--- src/hooks/useTrackViewport.js
+++ src/hooks/useTrackViewport.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
 import { useLocation } from 'react-router-dom';
 
 export function useTrackViewport(links) {
@@ -7,37 +6,13 @@ export function useTrackViewport(links) {
     const [currentSection, setCurrentSection] = useState(0);
     const location = useLocation();
 
-    const {refs, entries} = links.reduce((result, current, index) => {
-        // Return ref only for elements
-        if(current.route.includes('#')) {
-            const { ref, entry } = useInView({
-                threshold: 0.45,
-            });
-            result.refs[`${current.name.toLowerCase()}Ref`] = ref;
-            result.entries.push({
-                isIntersecting: entry?.isIntersecting,
-                index
-            });
-            return result; 
-        }
-        return result;
-    }, {
-        refs: {}, entries: []
-    });
-
     useEffect(() => {
-        const currentElement = entries.filter(entry => entry.isIntersecting)[0];
-        if (currentElement) {
-            setCurrentSection(currentElement.index);
-        } else {
-            const currentPage = links.map(link => link.route).indexOf(location.pathname);
-            if(currentPage < 0) return;
-            setCurrentSection(currentPage);
-        }
-    }, [entries, location]);
+        const currentPage = links.map(link => link.route).indexOf(location.pathname.slice(1));
+        if(currentPage < 0) return;
+        setCurrentSection(currentPage);
+    }, [location]);
 
     return {
         currentSection,
-        refs
     }
 }
