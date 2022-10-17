5b5a937dffae3857e5514cd62465bc38d6956b29
Tsubasa <tsubasanikander@gmail.com>
2022-10-17 17:36:20 +0200

    Add lazy loader

--- src/routers/Projects/index.js
+++ src/routers/Projects/index.js
@@ -1,2 +1,14 @@
-import Projects from './Projects';
+import { lazy, Suspense } from 'react';
+import Spinner from '../../components/Spinner';
+
+const ProjectsPage = lazy(() => import('./Projects'));
+
+function Projects() {
+    return (
+        <Suspense fallback={<Spinner />}>
+            <ProjectsPage />
+        </Suspense>
+    );
+}
+
 export default Projects;
