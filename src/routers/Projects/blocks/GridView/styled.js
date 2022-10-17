7b72c143fae2f01dd16d05a24ce46537db5b946f
Tsubasa <tsubasanikander@gmail.com>
2022-10-17 17:49:46 +0200

    Add max-width to prevent overflow

--- src/routers/Projects/blocks/GridView/styled.js
+++ src/routers/Projects/blocks/GridView/styled.js
@@ -5,16 +5,17 @@ import { devices } from '../../../../static/viewport.js';
 
 export const Grid = styled(Content)`
     min-height: 50vh;
+    max-width: 100%;
     margin-top: ${({ theme }) => theme.spacing.md};
 
     display: grid;
-    grid-template-columns:  max-content;
+    //grid-template-columns:  max-content;
     grid-template-rows: max-content;
     gap: ${({ theme }) => `${theme.spacing.gap} ${theme.spacing.md}`};
 
     @media screen and (${devices.tablet}) {
         grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-        max-width: 100%;
+        //max-width: 100%;
     }
     @media screen and (${devices.desktop}) {
         gap: ${({ theme }) => `${theme.spacing.gap} ${theme.spacing.lg}`};
