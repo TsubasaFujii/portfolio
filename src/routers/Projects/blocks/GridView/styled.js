284a76dbf41f2f8bbd1c819a5cddd7a5c52e123d
Tsubasa <tsubasanikander@gmail.com>
2022-10-17 16:48:46 +0200

    Fix Grid Wrapper max width

--- src/routers/Projects/blocks/GridView/styled.js
+++ src/routers/Projects/blocks/GridView/styled.js
@@ -8,12 +8,13 @@ export const Grid = styled(Content)`
     margin-top: ${({ theme }) => theme.spacing.md};
 
     display: grid;
-    grid-template-columns: 100%;
+    grid-template-columns:  max-content;
     grid-template-rows: max-content;
     gap: ${({ theme }) => `${theme.spacing.gap} ${theme.spacing.md}`};
 
     @media screen and (${devices.tablet}) {
         grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+        max-width: 100%;
     }
     @media screen and (${devices.desktop}) {
         gap: ${({ theme }) => `${theme.spacing.gap} ${theme.spacing.lg}`};
