d4b863c6e36b385ddf4c1567c85e0f880f8ad9ab
Tsubasa <tsubasanikander@gmail.com>
2022-10-27 17:20:28 +0200

    Add max width

--- src/routers/Projects/blocks/GridView/styled.js
+++ src/routers/Projects/blocks/GridView/styled.js
@@ -17,6 +17,7 @@ export const Grid = styled(Content)`
         grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
     }
     @media screen and (${devices.desktop}) {
+        max-width: ${({ theme }) => theme.max.width};
         gap: ${({ theme }) => `${theme.spacing.gap} ${theme.spacing.lg}`};
     }
 `;
