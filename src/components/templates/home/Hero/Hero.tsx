d9873d2ab9933727550277892d772cf5ed489ebd
Tsubasa <tsubasanikander@gmail.com>
2023-04-28 21:17:02 +0200

    Merge background comp to Hero

--- src/components/templates/home/Hero/Hero.tsx
+++ src/components/templates/home/Hero/Hero.tsx
@@ -5,10 +5,9 @@ import { scrollTo } from '@/utils/window';
 
 import { SubHeading } from './SubHeading';
 
-import BackgroundLayer from './BackgroundLayer';
-import { Heading } from './Heading';
-import { ContentWrapper, Wrapper } from './styled';
 import { Button } from '@/components/common';
+import { Heading } from './Heading';
+import { BackgroundWrapper, ContentWrapper, MotionCircle, Wrapper } from './styled';
 
 export default function Hero() {
     const [headerHeight, setHeaderHeight] = useState<number | null>(null);
@@ -42,7 +41,34 @@ export default function Hero() {
                     icon='chevronDown'
                     onClick={handleOnClick} />
             </ContentWrapper>
-            <BackgroundLayer isVisible={inView} headerHeight={headerHeight} />
+            <BackgroundWrapper>
+                <svg
+                    viewBox='0 0 100 100'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <MotionCircle
+                        initial={{
+                            cx: '50%',
+                            cy: '55%',
+                            scale: 0.5,
+                        }}
+                        animate={{
+                            cx: inView ? '80%' : 0,
+                            scale: 1,
+                            transition: {
+                                default: {
+                                    type: 'spring',
+                                    damping: 8,
+                                    stiffness: 80,
+                                },
+                                scale: {
+                                    duration: 0.5,
+                                }
+                            },
+                        }}
+                        r='40%'
+                    />
+                </svg>
+            </BackgroundWrapper>
         </Wrapper >
     )
 }
--- src/components/templates/home/Hero/styled.tsx
+++ src/components/templates/home/Hero/styled.tsx
@@ -1,5 +1,6 @@
-import { Content, SectionRef } from '@/components/common';
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
+import { Content, SectionRef } from '@/components/common';
 
 export const Wrapper = styled(SectionRef) <{ $headerHeight: number | null; }>`
     min-height: ${({ $headerHeight }) => `calc(100vh - ${$headerHeight}px)`};
@@ -11,4 +12,22 @@ export const Wrapper = styled(SectionRef) <{ $headerHeight: number | null; }>`
 
 export const ContentWrapper = styled(Content)`
     align-items: flex-start;
+`;
+
+export const BackgroundWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    z-index: -1;
+    overflow: visible;
+
+    & svg {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+export const MotionCircle = styled(motion.circle)`
+    fill: ${({ theme }) => theme.colors.primary};
 `;
