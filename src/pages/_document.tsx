d9919f34d608e10d4acb00c604af7fc8b825a580
Tsubasa <tsubasanikander@gmail.com>
2023-06-17 11:01:48 +0200

    Add serverStyledSheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+export default class CustomDocument extends Document {
+    static async getInitialProps(context: any) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = context.renderPage
+
+        try {
+            context.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App: any) => (props: any) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(context)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
 }
